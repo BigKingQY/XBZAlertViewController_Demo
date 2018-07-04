353699a6f7e4fd0a74c003dfbcd0930f5c005e0c
BigKing <641648019@qq.com>
2018-07-04 15:08:41 +0800

    Initial Commit

---
+++ XBZAlertViewController_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBZAlertViewController_Demo
+//
+//  Created by BigKing on 2018/7/4.
+//  Copyright © 2018年 BigKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XBZAlertViewController_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XBZAlertViewController_Demo
+//
+//  Created by BigKing on 2018/7/4.
+//  Copyright © 2018年 BigKing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
